71458be8d2863573455a24a101d5c9ded689872d
Aitor708 <xxeduar4xx@gmail.com>
2026-02-01 19:08:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Yeiiiii I knew!";
 
     catImg.src = "cat_dance.gif";
 
